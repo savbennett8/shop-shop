a30baa7cfbc95a162eae080ab30c50feadab5ce6
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-17 18:54:27 -0600

    cart static display on home and single product pages

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,6 +6,7 @@ import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../utils/actions";
+import Cart from '../components/Cart';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -60,6 +61,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
