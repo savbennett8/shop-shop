dcf542c20912b0a12b5592793399727e122641ef
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-22 13:34:24 -0600

    added items in the cart are retrieved on browser re-open

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -31,11 +31,20 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+
+      //if updating qty, use existing item data & increment purchaseQty by 1
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
+
+      //if product isn't in the cart yet, add it to the current idb shopping cart
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
@@ -44,6 +53,8 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   useEffect(() => {
